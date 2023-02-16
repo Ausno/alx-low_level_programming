3932512af7ec5e75d9e8c20c13327b190ffa3af5
Onuoha Chidi <onuohachidi13@gmail.com>
2023-02-16 10:45:16 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - A program that print a line using the print f function
  * Return: 0
  */
- int main(void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
