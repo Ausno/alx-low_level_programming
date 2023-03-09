94f33dd3d3c95cfce968ead57fb590c08e4ca690
Onuoha Chidi <onuohachidi13@gmail.com>
2023-03-09 13:07:50 -0800

    Updated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,9 +5,12 @@ i#include "main.h"
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		return;
 	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
